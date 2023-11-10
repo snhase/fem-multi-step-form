466b37361801e3e2b90ee8875265812a0636790e
snhase <snehal.hase@gmail.com>
2023-11-09 18:54:14 -0800

    add card layout and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { ProgressIndicator } from './components/ProgressIndicator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="main-content">
+        <div className="card">
+          <div className='card-style'>
+            <div className='sidebar-bg'>
+            </div>
+            <div className='card-content'>
+      
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='footer'>
+        Challenge by <a 
+            href="https://www.frontendmentor.io/challenges/multistep-form-YVAnSdqQBJ" 
+            alt="frontend-mentor challenge link" 
+            target="_blank" 
+            rel="noopener noreferrer">Frontend Mentor</a>. Coded by <a href="https://github.com/snhase" alt="github url" target="_blank" rel="noopener noreferrer">snhase</a></div>
     </div>
   );
 }
